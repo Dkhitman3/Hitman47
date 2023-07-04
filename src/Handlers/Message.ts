8e89bc57cdf13fbc990a96e00286174ee20af947
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-07-04 20:38:46 +0200

    Update Message.ts

--- src/Handlers/Message.ts
+++ src/Handlers/Message.ts
@@ -130,7 +130,7 @@ export class MessageHandler {
             await this.client.DB.updateUser(M.sender.jid, 'tag', 'set', this.client.utils.generateRandomUniqueTag())
         const cmd = args[0].toLowerCase().slice(prefix.length)
         const command = this.commands.get(cmd) || this.aliases.get(cmd)
-        if (!command) return void M.reply('*🟥Do you mean #help*')
+        if (!command) return void M.reply('*🚫Do you mean help*')
         const disabledCommands = await this.client.DB.getDisabledCommands()
         const index = disabledCommands.findIndex((CMD) => CMD.command === command.name)
         if (index >= 0)
