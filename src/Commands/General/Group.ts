cbdbe1d42b127312ae46fccbc01694cab3d37393
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-07-06 15:29:14 +0200

    Update Group.ts

--- src/Commands/General/Group.ts
+++ src/Commands/General/Group.ts
@@ -23,7 +23,7 @@ export default class extends BaseCommand {
         text += `*Subject*: ${subject}\n`
         text += `*Owner*: ${owner}\n`
         text += `*Participants*: ${participants.length}\n`
-        text += `*Admins*: ${admins.length}\n`
+        text += `*Admins*: ${admins.length ?? 0}\n`
         text += `*NSFW*: ${nsfw}\n`
         text += `*Mods*: ${mods}\n`
         text += `*Events*: ${events}`
@@ -35,4 +35,4 @@ export default class extends BaseCommand {
             text
         ))
     }
-}
+ }
