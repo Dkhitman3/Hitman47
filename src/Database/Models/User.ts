b24f02e0773f160d745d16ea12dbe0fa3984d0d4
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-07-19 08:57:29 +0200

    Update User.ts

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -2,43 +2,6 @@ import { prop, getModelForClass } from '@typegoose/typegoose'
 import { Document } from 'mongoose'
 import { ICharacter, ICharacter as WaifuResponse } from '@shineiichijo/marika'
 
-export class Pokemon {
-    @prop({ type: String, required: true })
-    public name!: string
-
-    @prop({ type: String, required: true })
-    public image!: string
-
-    @prop({ type: Number, required: true })
-    public id!: number
-
-    @prop({ type: Number, required: true })
-    public level!: number
-}
-
-class Gallery implements ICharacter {
-    @prop({ required: true })
-    public mal_id!: ICharacter['mal_id']
-
-    @prop({ type: String, required: true })
-    public url!: string
-
-    @prop({ type: Object, required: true })
-    public images!: ICharacter['images']
-
-    @prop({ type: String, required: true })
-    public name!: string
-
-    @prop({ type: () => [String], required: true, default: [] })
-    public nicknames!: string[]
-
-    @prop({ type: String, required: true })
-    public about!: string
-
-    @prop({ required: true })
-    public favorites!: number
-}
-
 class Inventory {
     @prop({ type: String, required: true })
     public item!: string
@@ -99,12 +62,6 @@ export class UserSchema {
     public tag!: string
 
     @prop({ type: Number, required: true, default: 0 })
-    public wallet!: number
-
-    @prop({ type: Number, required: true, default: 0 })
-    public bank!: number
-
-    @prop({ type: Number, required: true, default: 0 })
     public quizWins!: number
 
     @prop({ type: () => Haigusha, required: true, default: () => ({ married: false, data: {} }) })
@@ -112,12 +69,6 @@ export class UserSchema {
 
     @prop({ type: () => Icon, required: true, default: () => ({ custom: false }) })
     public icon!: Icon
-
-    @prop({ type: Number, required: true, default: 0 })
-    public lastDaily!: number
-
-    @prop({ type: Number, required: true, default: 0 })
-    public lastRob!: number
 
     @prop({ type: () => About, required: true, default: () => ({ custom: false }) })
     public about!: About
@@ -130,12 +81,6 @@ export class UserSchema {
 
     @prop({ type: String, required: true, default: 'None' })
     public companion!: string
-
-    @prop({ type: () => Pokemon, required: true, default: [] })
-    public party!: Pokemon[]
-
-    @prop({ type: () => Pokemon, required: true, default: [] })
-    public pc!: Pokemon[]
 
     @prop({ type: () => Gallery, required: true, default: [] })
     public gallery!: Gallery[]
