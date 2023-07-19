984ea9e5ca316b6ccce85ad67d943890b72ce6cc
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-07-19 17:43:14 +0200

    Update Leave.ts

--- src/Commands/Dev/Leave.ts
+++ src/Commands/Dev/Leave.ts
@@ -9,7 +9,10 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message): Promise<void> => {
+        const bye = [`*Goodbye* 👋`, 'Peace out', 'goodbye', 'I’ve got to get going', 'I must be going']
+        const rand = this.client.utils.getRandomInt(1, bye.length - 1)
+        await M.reply(bye[rand])
         const jid = M.from
         await this.client.groupLeave(jid)
     }
-}
+ }
