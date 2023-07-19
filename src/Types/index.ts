b875fd2b479564e41d940300d23505d58269afcf
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-07-19 09:02:08 +0200

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -52,12 +52,8 @@ export interface YT_Search {
     }
 }
 
-export type TGroupFeature = 'events' | 'nsfw' | 'chara' | 'wild' | 'mods'
-
 export enum GroupFeatures {
     'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
-    'wild' = 'By enabling this feature, it will send wild pokemon',
-    'chara' = 'By enabling this feature, it will send characters',
     'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
     'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
 }
