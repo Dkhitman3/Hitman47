e87e1be6bd4cdee623e5ac7b95d70c06a9fc1458
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-08-03 08:08:48 +0200

    Update Demote.ts

--- src/Commands/Moderation/Demote.ts
+++ src/Commands/Moderation/Demote.ts
@@ -9,26 +9,21 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class command extends BaseCommand {
     override execute = async (M: Message): Promise<void> => {
-        if (!M.groupMetadata) return void M.reply('*Try Again!*')
-        const users = M.mentioned
-        if (M.quoted && !users.includes(M.quoted.sender.jid)) users.push(M.quoted.sender.jid)
-        if (!users.length || users.length < 1) return void M.reply('users?')
-        const mentioned = users
-        let text = ''
-        for (const user of users) {
-            const i = users.indexOf(user)
-            if (i === 0) text += '\n'
-            if (user === M.groupMetadata.owner || '') {
-                text += `Skipped @${user.split('@')[0]} as they're the owner`
-                continue
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender.jid)
+        M.mentioned = [...new Set(M.mentioned)]
+        if (M.mentioned.length === 0) return void (await M.reply(`🟥 *Mentions are required to demote*`))
+        if (M.mentioned.length > 5)
+            M.reply(`🟥 *You can only demote up to 5 users at a time, Remove some users and try again*`)
+        let text = `🎖️ Demoting Users...\n`
+        for (const jid of M.mentioned) {
+            const number = jid.split('@')[0]
+            if (!M.group?.admins?.includes(jid)) text += `\n🟨 *@${number}* is already an not admin`
+            else {
+                await this.client.groupParticipantsUpdate(M.from, [jid], 'demote')
+                text += `\n🟩 Demoted *@${number}*`
             }
-            if (!M.groupMetadata.admins?.includes(user)) {
-                text += `Skipped @${user.split('@')[0]} as they're not admin`
-                continue
-            }
-            await this.client.groupParticipantsUpdate(M.from, [user], 'demote')
-            text += `\n *🔴you're fired!* @${user.split('@')[0]}`
         }
-        return void M.reply(text, 'text', undefined, undefined, undefined, mentioned)
+        await M.reply(text, undefined, undefined, undefined, M.mentioned)
     }
-}
+ }
+
