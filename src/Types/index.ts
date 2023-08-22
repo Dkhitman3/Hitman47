2b0d2ae6426aa639f5c50615aa8172172e432228
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-08-22 17:18:24 +0200

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -3,7 +3,6 @@ import Baileys, { GroupMetadata, ParticipantAction } from '@whiskeysockets/baile
 export * from './Config'
 export * from './Command'
 export * from './Message'
-export * from './Pokemon'
 
 export interface IContact {
     jid: string
@@ -53,9 +52,7 @@ export interface YT_Search {
 }
 
 export enum GroupFeatures {
-    'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
-    'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
-    'nsfw' = 'By enabling this feature, it enables the bot to send *NSFW* contents'
+    'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted'
 }
 
 export interface IGroup extends GroupMetadata {
