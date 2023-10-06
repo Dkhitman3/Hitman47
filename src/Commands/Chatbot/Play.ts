a285c2ff178d8497267df4f58b11bf4084ec6e6d
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-10-06 17:51:52 +0200

    Update Play.ts

--- src/Commands/Chatbot/Play.ts
+++ src/Commands/Chatbot/Play.ts
@@ -3,28 +3,28 @@ import { Command, BaseCommand, Message } from '../../Structures'
 import { IArgs, YT_Search } from '../../Types'
 
 @Command('play', {
-    description: 'Plays a song of the given term from YouTube',
-    cooldown: 35,
+    description: 'download a song of the given term from YouTube',
+    cooldown: 1,
     exp: 35,
-    aliases: ['song', 'download', 'music'],
-    dm: true,
-    category: 'chatbot',
+    aliases: ['doc'],
+    category: 'media',
     usage: 'play [term]'
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
         if (!context) return void M.reply('Provide a term to play, Baka!')
         const term = context.trim()
-        M.reply('*..Please wait Im downloading your song...*')
+        M.reply('🧧...Downloading...🧧')
         const videos = await this.client.utils.fetch<YT_Search[]>(`https://weeb-api.vercel.app/ytsearch?query=${term}`)
         if (!videos || !videos.length) return void M.reply(`No matching songs found | *"${term}"*`)
         const buffer = await new YT(videos[0].url, 'audio').download()
-        return void (await M.reply(buffer, 'audio', undefined, 'audio/mpeg', undefined, undefined, {
+        return void (await M.reply(buffer, 'document', undefined, 'audio/mpeg', undefined, undefined, {
             title: videos[0].title,
             thumbnail: await this.client.utils.getBuffer(videos[0].thumbnail),
             mediaType: 2,
             body: videos[0].description,
             mediaUrl: videos[0].url
-        }))
+        }, undefined, `${decodeURI(videos[0].title)}.mp3`))
     }
-}
+ }
+
