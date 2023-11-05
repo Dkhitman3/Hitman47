1aea46b6806fa4e10ca15b6bd777bcc8c6be25e1
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-11-05 12:41:24 +0200

    Update Play.ts

--- src/Commands/Chatbot/Play.ts
+++ src/Commands/Chatbot/Play.ts
@@ -8,7 +8,7 @@ import { IArgs, YT_Search } from '../../Types'
     exp: 35,
     dm: true,
     aliases: ['doc'],
-    category: 'dev',
+    category: 'chatbot',
     usage: 'play [term]'
 })
 export default class extends BaseCommand {
