f501d16052509840a8cf85e810944537ad8b18d2
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-11-13 20:37:03 +0200

    Update User.ts

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -1,6 +1,24 @@
 import { prop, getModelForClass } from '@typegoose/typegoose'
 import { Document } from 'mongoose'
 
+class Ban {
+    @prop({ type: Boolean, required: true, default: false })
+    public banned!: boolean
+
+    @prop({ type: String })
+    public reason?: string
+
+    @prop({ type: String })
+    public bannedBy?: string
+
+    @prop({ type: String })
+    public bannedIn?: string
+
+    @prop({ type: String })
+    public time?: string
+    
+}
+
 export class UserSchema {
     @prop({ type: String, required: true, unique: true })
     public jid!: string
