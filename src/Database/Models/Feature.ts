b6c72b53fd6c55f3e0ceeb5b31defada4bb60066
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-11-15 12:33:47 +0200

    Update Feature.ts

--- src/Database/Models/Feature.ts
+++ src/Database/Models/Feature.ts
@@ -7,29 +7,6 @@ export class FeatureSchema {
 
     @prop({ type: Boolean, required: true, default: false })
     public state!: boolean
-
-    @prop({ type: () => Items, required: true })
-    public items!: Items[]
-}
-
-class Items {
-    @prop({ type: Number, required: true })
-    public id!: number
-
-    @prop({ type: String, required: true })
-    public emoji!: string
-
-    @prop({ type: String, required: true })
-    public name!: string
-
-    @prop({ type: Number, required: true })
-    public description!: string
-
-    @prop({ type: Number, required: true })
-    public usageLimit!: number
-
-    @prop({ type: Number, required: true })
-    public price!: number
 }
 
 export type TFeatureModel = FeatureSchema & Document
