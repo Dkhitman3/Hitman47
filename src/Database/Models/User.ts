bc9c4841d7fe49cae50be6b3c3bbcb1627444361
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-18 07:21:21 +0200

    Update User.ts

--- src/Database/Models/User.ts
+++ src/Database/Models/User.ts
@@ -1,29 +1,18 @@
 import { prop, getModelForClass } from '@typegoose/typegoose'
 import { Document } from 'mongoose'
 
-class Ban {
-    @prop({ type: Boolean, required: true, default: false })
-    public banned!: boolean
-
-    @prop({ type: String })
-    public reason?: string
-
-    @prop({ type: String })
-    public bannedBy?: string
-
-    @prop({ type: String })
-    public bannedIn?: string
-
-    @prop({ type: String })
-    public time?: string  
-}
-
 export class UserSchema {
     @prop({ type: String, required: true, unique: true })
     public jid!: string
 
-    @prop({ type: () => Ban, required: true, default: () => ({ banned: false }) })
-    public ban!: Ban
+    @prop({ type: Number, required: true, default: 0 })
+    public experience!: number
+
+    @prop({ type: Boolean, required: true, default: false })
+    public banned!: boolean
+
+    @prop({ type: Number, required: true, default: 1 })
+    public level!: number
 
     @prop({ type: String, required: true })
     public tag!: string
