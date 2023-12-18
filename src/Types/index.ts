d2911829b3a32ff949f6ca9795ab8be68121ae3e
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-18 09:48:38 +0200

    Update index.ts

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -52,8 +52,10 @@ export interface YT_Search {
 }
 
 export enum GroupFeatures {
-    'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted'
-}
+    'events' = 'By enabling this feature, the bot will welcome new members, gives farewell to the members who left the group or were removed and reacts when a member is promoted or demoted',
+    'mods' = "By enabling this feature, it enables the bot to remove the member (except for admins) which sent an invite link of other groups. This will work if and only if the bot's an admin",
+    'porn' = 'By enabling this feature, it enables the bot to send *porn* contents'
+ }
 
 export interface IGroup extends GroupMetadata {
     admins?: string[]
