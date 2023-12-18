6e4aaada0d8ab24e9432ff2a0b3e6f0f49e162b0
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-18 11:19:43 +0200

    Update Client.ts

--- src/Structures/Client.ts
+++ src/Structures/Client.ts
@@ -20,6 +20,7 @@ export class Client extends (EventEmitter as new () => TypedEventEmitter<Events>
             name: process.env.BOT_NAME || 'bot',
             session: process.env.SESSION || 'SESSION',
             prefix: process.env.PREFIX || '#',
+            gkey: 'AIzaSyDMbI3nvmQUrfjoCJYLS69Lej1hSXQjnWIEcx',
             mods: (process.env.MODS || '27844132352').split(', ').map((user) => `${user}@s.whatsapp.net`),
             chatBotUrl: 'http://api.brainshop.ai/get?bid=170305&key=8OpWeiccHtCb1dFj&uid=[uid]&msg=[msg',
             PORT: Number(process.env.PORT || 3000)
