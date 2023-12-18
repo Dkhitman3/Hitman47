db2fa00b81b522296e926ba3a25dc8a2dfdbbfce
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-18 07:34:41 +0200

    Update Broadcast.ts

--- src/Hitman47/Boss/Broadcast.ts
+++ src/Hitman47/Boss/Broadcast.ts
@@ -1,56 +1,48 @@
-import { IArgs } from '../../Types';
-import { Command, BaseCommand, Message } from '../../Structures';
-import { AnyMessageContent, proto, delay } from '@whiskeysockets/baileys';
+import { IArgs } from '../../Types'
+import { Command, BaseCommand, Message } from '../../Structures'
+import { AnyMessageContent, proto, delay } from '@adiwajshing/baileys'
 
 @Command('broadcast', {
     description: '',
     aliases: ['bc'],
-    category: 'boss',
+    category: 'dev',
     usage: ''
 })
-export default class BroadcastCommand extends BaseCommand {
-    override async execute(M: Message, { context }: IArgs, arr: string[] = []): Promise<void> {
+export default class command extends BaseCommand {
+    override execute = async (M: Message, { context }: IArgs, arr: string[] = []): Promise<void> => {
         try {
-            let type: 'text' | 'image' | 'video' = 'text';
-            let buffer!: Buffer;
+            let type: 'text' | 'image' | 'video' = 'text'
+            let buffer!: Buffer
             if (M.hasSupportedMediaMessage) {
-                if (M.type === 'imageMessage') type = 'image';
-                if (M.type === 'videoMessage') type = 'video';
-                buffer = await M.downloadMediaMessage(M.message.message as proto.IMessage);
+                if (M.type === 'imageMessage') type = 'image'
+                if (M.type === 'videoMessage') type = 'video'
+                buffer = await M.downloadMediaMessage(M.message.message as proto.IMessage)
             } else if (!M.hasSupportedMediaMessage && M.quoted && M.quoted.hasSupportedMediaMessage) {
-                if (M.quoted.type === 'imageMessage') type = 'image';
-                if (M.quoted.type === 'videoMessage') type = 'video';
-                buffer = await M.downloadMediaMessage(M.quoted.message as proto.IMessage);
+                if (M.quoted.type === 'imageMessage') type = 'image'
+                if (M.quoted.type === 'videoMessage') type = 'video'
+                buffer = await M.downloadMediaMessage(M.quoted.message as proto.IMessage)
             }
-
-            let caption: string;
-            if ((!context && !M.quoted?.content) || (!context && M.quoted?.content === '')) {
-                return void M.reply('🟥 *Provide the text to be broadcasted*');
-            }
-
-            if (context) {
-                caption = context.trim();
-            } else {
-                caption = M.quoted?.content.trim() as string;
-            }
-
-            if (!arr.length) {
-                arr = await this.client.getAllGroups();
-            }
-            const text = `*Hitman47*\n\n🍁 *name:* *${M.sender.username}*\n\n${caption}`;
+            let caption: string
+            if ((!context && !M.quoted?.content) || (!context && M.quoted?.content === ''))
+                return void M.reply('🟥 *Provide the text to be broadcasted*')
+            if (context) caption = context.trim()
+            else caption = M.quoted?.content.trim() as string
+            if (!arr.length) arr = await this.client.getAllGroups()
+            const text = `*「 BOT 」*\n\n🍁 *Author:* *${M.sender.username}*\n\n${caption}`
             for (const group of arr) {
-                await delay(5000); 
+                await delay(5000)
                 await this.client.sendMessage(group, {
                     [type]: type === 'text' ? text : buffer,
                     caption: type === 'text' ? undefined : text,
                     gifPlayback: type === 'video' ? true : undefined,
                     jpegThumbnail: type === 'image' ? buffer.toString('base64') : undefined
-                } as unknown as AnyMessageContent);
+                } as unknown as AnyMessageContent)
+                arr.splice(arr.indexOf(group), 1)
             }
-            return void M.reply('🟢successful broadcasting🟢!');
+            return void M.reply('Done!')
         } catch {
-            // Handling errors by recursively calling execute function
-            return await this.execute(M, { context, flags: [], args: [] }, arr);
+            return await this.execute(M, { context, flags: [], args: [] }, arr)
         }
     }
-}
+    }
+            
