250d062e798ebdb57a8033f89d0296f6cdb69e3d
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-18 07:21:53 +0200

    Update Group.ts

--- src/Database/Models/Group.ts
+++ src/Database/Models/Group.ts
@@ -5,15 +5,17 @@ export class GroupSchema {
     @prop({ type: String, unique: true, required: true })
     public jid!: string
 
+    @prop({ type: Boolean, required: true, default: false })
+    public events!: boolean
+
     @prop({ type: String, required: true, default: 'all' })
     public bot!: string
 
     @prop({ type: Boolean, required: true, default: false })
-    public events!: boolean
-
-    @prop({ type: Boolean, required: true, default: false })
     public mods!: boolean
 
+    @prop({ type: Boolean, required: true, default: false })
+    public porn!: boolean
 }
 
 export type TGroupModel = GroupSchema & Document
