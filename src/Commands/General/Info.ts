cff602c371b7b7ce3e0d291cae6606a145b19fef
von-strucker <138525731+von-strucker@users.noreply.github.com>
2023-12-31 17:33:29 +0200

    Just

--- src/Commands/General/Info.ts
+++ src/Commands/General/Info.ts
@@ -10,6 +10,13 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     public override execute = async ({ reply }: Message): Promise<void> => {
+        const users = await this.client.DB.user.count()
+        let getGroups = await this.client.groupFetchAllParticipating()
+        let groups = Object.entries(getGroups)
+            .slice(0)
+            .map((entry) => entry[1])
+        let res = groups.map((v) => v.id)
+        console.log(res.length)
         const { description, name, homepage } = require(join(__dirname, '..', '..', '..', 'package.json')) as {
             description: string
             homepage: string
@@ -17,7 +24,7 @@ export default class extends BaseCommand {
         }
         const image = this.client.assets.get('hitman47') as Buffer
         const uptime = this.client.utils.formatSeconds(process.uptime())
-        const text = `*Hitman47* \n\n📡 *Description: ${description}*\n\n🛠️ *Commands:* ${this.handler.commands.size}\n\n🛡️ *Uptime:* ${uptime}`
+        const text = `*🍁 ${this.client.config.name} 🍁*\n\n📙 *Description: ${description}*\n\n🔗 *Commands:* ${this.handler.commands.size}\n🚦 *Uptime:* ${uptime}\n🎐 *Users:* ${users}\n🌃 *Mods:* ${this.client.config.mods.length}\n🔮 *Groups:* ${groups.length}`
         return void (await reply(image, 'image', undefined, undefined, text, undefined, {
             title: this.client.utils.capitalize(name),
             thumbnail: image,
@@ -25,4 +32,4 @@ export default class extends BaseCommand {
             sourceUrl: homepage
         }))
     }
-}
+            }
