c110bb5d4ab17f46e837777b8df2af57180ec1ff
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2023-12-31 18:15:46 +0200

    Update Enable-Disable.ts

--- src/Commands/Group/Enable-Disable.ts
+++ src/Commands/Group/Enable-Disable.ts
@@ -5,6 +5,7 @@ import { IArgs, GroupFeatures } from '../../Types'
 @Command('enable', {
     description: 'Enables/Disables a certain group feature',
     usage: 'set',
+    aliases: ['e', 'en', 'set'],
     cooldown: 2,
     category: 'moderation',
     exp: 25,
