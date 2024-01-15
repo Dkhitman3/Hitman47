42c70605762eff15c27218b52e0a237c546ba98e
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2024-01-15 12:52:14 +0200

    Create Facebook.ts

---
+++ src/Commands/Media/Facebook.ts
@@ -0,0 +1,52 @@
+import { BaseCommand, Command, Message } from '../../Structures';
+import axios from 'axios';
+
+@Command('facebook', {
+    description: 'Download Facebook content',
+    category: 'media',
+    usage: 'facebook <Facebook post URL>',
+    aliases: ['fb'],
+    exp: 20,
+    cooldown: 1
+})
+export default class extends BaseCommand {
+    public override execute = async ({ reply, urls }: Message): Promise<void> => {
+        if (!urls || !urls.length) {
+            return void (await reply('❌ Please provide a Facebook post URL'));
+        }
+
+        const [url] = urls;
+        if (!url.includes('facebook.com')) {
+            return void (await reply('❌ Wrong URL! Only Facebook post URLs can be downloaded'));
+        }
+
+        try {
+            add your Facebook Graph API using an access token
+            const accessToken = 'YOUR_FACEBOOK_ACCESS_TOKEN';
+            const postId = extractPostIdFromUrl(url);
+
+            const { data } = await axios.get(
+                `https://graph.facebook.com/v14.0/${postId}?fields=attachments&access_token=${accessToken}`
+            );
+            const attachments = data.attachments?.data;
+            
+            if (attachments && attachments.length > 0) {
+                const firstAttachment = attachments[0];
+                const attachmentUrl = firstAttachment.media.image.src;
+
+                const buffer = await this.client.utils.getBuffer(attachmentUrl);
+                await reply(buffer, 'image');
+
+            } else {
+                await reply(`❌ No media data found for the provided Facebook post URL.`);
+            }
+        } catch (error: any) {
+            await reply(`❌ Error while getting media data: ${(error as Error).message}`);
+        }
+    };
+
+    private extractPostIdFromUrl(url: string): string {
+      
+   example: https://www.facebook.com/{username}/posts/{post_id}
+    }
+}
