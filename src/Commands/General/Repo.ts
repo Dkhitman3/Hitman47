98ac8eb58228c348cd07eda1e39c2342a8fd9499
ChristianNimb <152479793+ChristianNimb@users.noreply.github.com>
2024-01-17 19:10:16 +0000

    Update Repo.ts

--- src/Commands/General/Repo.ts
+++ src/Commands/General/Repo.ts
@@ -10,12 +10,12 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message): Promise<void> => {
-        const image = await this.client.utils.getBuffer('https://telegra.ph/file/bdd281b87aaa6f4ee0a67.jpg')
+        const image = await this.client.utils.getBuffer('https://telegra.ph/file/7d99d1210ce607f428782.jpg')
         
         let text = ''
-        text += `*Hitman47* ✨\n\n`
+        text += `*Godspeed* ✨\n\n`
         text += `*fork and give a star 🌟 so you can get more updates* \n\n`
-        text += `⚙️ *Repo Link: https://github.com/Dkhitman3/Hitman47*`
+        text += `⚙️ *Repo Link: https://github.com/ChristianNimb/Godspeed*`
         return void (await M.reply(image, 'image', undefined, undefined, text))
     }
 }
