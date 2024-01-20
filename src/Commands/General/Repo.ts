dbfb2e6926c6b85486fa05fc672d55f2a3a6e736
GODSPEED <152479793+ChristianNimb@users.noreply.github.com>
2024-01-20 17:45:51 +0000

    Update Repo.ts

--- src/Commands/General/Repo.ts
+++ src/Commands/General/Repo.ts
@@ -10,12 +10,12 @@ import { BaseCommand, Command, Message } from '../../Structures'
 })
 export default class extends BaseCommand {
     public override execute = async (M: Message): Promise<void> => {
-        const image = await this.client.utils.getBuffer('https://telegra.ph/file/7d99d1210ce607f428782.jpg')
+        const image = await this.client.utils.getBuffer('https://telegra.ph/file/5ad3eba6ddfaa9607366b.jpg')
         
         let text = ''
         text += `*Godspeed* ✨\n\n`
         text += `*fork and give a star 🌟 so you can get more updates* \n\n`
-        text += `⚙️ *Repo Link: https://github.com/ChristianNimb/Godspeed*`
+        text += `⚙️ *Repo Link: *https://github.com/Dkhitman3/Hitman47`
         return void (await M.reply(image, 'image', undefined, undefined, text))
     }
 }
