05df0928640ace24dc5c6f007b854f8b2860cc9d
Jflex19 <159693483+Jflex19@users.noreply.github.com>
2024-02-11 19:03:15 +0300

    Create Support.ts

---
+++ src/Commands/General/Support.ts
@@ -0,0 +1,28 @@
+import { BaseCommand, Command, Message } from '../../Structures'
+import { AnyMessageContent } from '@whiskeysockets/baileys'
+
+@Command('support', {
+    description: 'the bot sends the support links to your pm.',
+    category: 'general',
+    usage: 'support',
+    cooldown: 10,
+    exp: 10,
+    dm: true
+})
+export default class command extends BaseCommand {
+    override execute = async (M: Message): Promise<void> => {
+        let url!: string
+        let text = '*📮SUPPORT GROUP LINKS*\n\n'
+        const { https://chat.whatsapp.com/HbQRhkPZfcnFQ0wA8bKVaT } = this.client.config
+        for (let i = 0; i < supportGroups.length; i++) {
+            const { subject } = await this.client.groupMetadata(supportGroups[i])
+            const code = await this.client.groupInviteCode(supportGroups[i])
+            text += `*#${i + 1}*\n*${subject}:* *https://chat.whatsapp.com/${code}*\n`
+            if (!url) url = `https://chat.whatsapp.com/${code}`
+        }
+        await this.client.sendMessage(M.sender.jid, {
+            text
+        } as unknown as AnyMessageContent)
+        return void M.reply('📪Sent the support group links in your DM.')
+    }
+}
