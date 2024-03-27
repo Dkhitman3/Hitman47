7412e703e23dea617fbcd518850e8ee7683149e6
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2024-03-27 09:20:04 +0200

    Update Group.ts

--- src/Database/Models/Group.ts
+++ src/Database/Models/Group.ts
@@ -15,6 +15,9 @@ export class GroupSchema {
     public bot!: string
 
     @prop({ type: Boolean, required: true, default: false })
+    public chara!: boolean
+
+    @prop({ type: Boolean, required: true, default: false })
     public mods!: boolean
 
 }
