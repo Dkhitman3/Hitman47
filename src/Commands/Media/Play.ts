ffb3b78a66dee9f18fce1baefb2ffb46f12d5857
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2024-10-03 07:03:24 +0200

    Update Play.ts

--- src/Commands/Media/Play.ts
+++ src/Commands/Media/Play.ts
@@ -13,7 +13,7 @@ export default class extends BaseCommand {
     public override execute = async (M: Message, { context }: IArgs): Promise<void> => {
         if (!context) return void M.reply('Provide a term to play, Baka!')
         const term = context.trim()
-        const videos = await this.client.utils.fetch<YT_Search[]>(`https://d-mail-api.vercel.app/ytsearch?query=${term}`)
+        const videos = await this.client.utils.fetch<YT_Search[]>(`${this.client.config.API_URL}download?url=${videos[0].url}&type=audio`)
         if (!videos || !videos.length) return void M.reply(`No matching songs found | *"${term}"*`)
         const buffer = await new YT(videos[0].url, 'audio').download()
         return void (await M.reply(buffer, 'audio', undefined, 'audio/mpeg', undefined, undefined, {
